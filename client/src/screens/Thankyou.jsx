a12aa0eb42fcb88a1564abad9d78fe13d7b411df
Suvish23 <suvishjain@gmail.com>
2020-12-02 20:33:49 +0530

    all done...

--- client/src/screens/Thankyou.jsx
+++ client/src/screens/Thankyou.jsx
@@ -1,9 +1,12 @@
 import React,{useContext} from 'react'
 import {UserContext} from '../userContext'
-import Header from '../components/Header';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import Footer from '../components/Footer';
 import { Grid} from '@material-ui/core';
+import { useHistory } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
     root: {
@@ -15,20 +18,37 @@ const useStyles = makeStyles({
         justify: 'center',
         alignContent: 'flex-end',
       },
+      typographyStyles: {
+        position: 'fixed',
+        left: '43.5vw',
+      },
   });
 
 
 
 export default function Thankyou() {
     const classes = useStyles();
+    const history = useHistory()
     const { userstore }= useContext(UserContext);
+    const onclickbackhandler = (e) =>{
+      e.preventDefault();
+        history.push('/Gallery');
+    }
     return (
         <Grid container direction="column" className={classes.root}>
+        <Grid item >
+    <AppBar position="static">
+      <Toolbar>
+          <Button  style={{color:'white'}} onClick={onclickbackhandler} ><ArrowBackOutlinedIcon  style={{color:'white', fontSize:'28px'}}/><h5>Back to Gallary</h5></Button>
+
+        <Typography className={classes.typographyStyles}>
+            <h2>SAMSUNG</h2>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </Grid>
         <Grid>
-          <Header/>  
-        </Grid>
-        <Grid>
-            <h2 style={{paddingLeft:'35vw',fontSize:'40px'}}>Thank you for shopping {userstore.user}.....</h2>
+            <h2 style={{paddingLeft:'30vw',paddingTop:"10vh",fontSize:'40px'}}>Thank you for shopping {userstore.user}.....</h2>
         </Grid>
         <Grid item container className={classes.footer}>
             <Footer/>
